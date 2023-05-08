f9d49e7b7d35313c13ae50ceca30a8d6684ea652
badgeget <132890504+badgeget@users.noreply.github.com>
2023-05-08 16:30:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,5 +12,5 @@ streamlit.text('🥑🍞Avocado toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 fruit_listed = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-foo = fruit_listed.dtypes.astype(str)
+foo = fruit_listed.astype(str)
 streamlit.write(foo)
