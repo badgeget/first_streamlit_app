166993313b6f6f62de1953951c12bcadfc6ac6b2
badgeget <132890504+badgeget@users.noreply.github.com>
2023-05-09 10:48:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 foo = fruits_to_show.astype(str)
 streamlit.write(foo)
 
-def get_fruityvice_data(this_fruit_choice);
+def get_fruityvice_data(this_fruit_choice)
    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
    return fruityvice_normalized
