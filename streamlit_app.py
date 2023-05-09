7880afd9d7045907af61bf9b80ad1685c36c7086
badgeget <132890504+badgeget@users.noreply.github.com>
2023-05-09 09:54:21 +0700

    Update streamlit_app.py
    import  request

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 
 
@@ -23,3 +24,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 foo = fruits_to_show.astype(str)
 streamlit.write(foo)
+
+streamlit.header("Fruityvice Fruit Advice!")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
